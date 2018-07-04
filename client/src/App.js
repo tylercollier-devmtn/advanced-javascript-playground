2d71cd04740c7852019a7c586b42e6a3989a4a5e
Tyler Collier <tyler.collier+github@devmounta.in>
2018-07-04 12:01:12 -0700

    Use setState with callback

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: 0,
+      counter2: 0,
+    };
+  }
+
+  increment = () => {
+    // this.setState({ counter: this.state.counter + 1 })
+    // this.incrementBy5()
+    // this.setState(prevState => {
+    //   return { counter: prevState.counter + 1 }
+    // })
+    this.setState(prevState => ({ counter: prevState.counter + 1 }))
+    // this.setState(prevState => {
+    //   return { counter: prevState.counter + 1 }
+    // })
+  }
+
+  incrementBy5 = () => {
+    this.setState({ counter: this.state.counter + 5 })
+  }
+  
   render() {
     return (
       <div className="App">
@@ -13,6 +37,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={this.increment}>Click me</button>
+        <div>Counter value: {this.state.counter}</div>
+        <div>Counter2 value: {this.state.counter2}</div>
       </div>
     );
   }
